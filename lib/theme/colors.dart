06f83ef788713a17b2ae083a2b7fe7e07b8f96d3
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-10 14:12:49 +0630

    Add NeonColors class with color constants

---
+++ lib/theme/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NeonColors {
+  // Neon Colors
+  static const Color neonCyan = Color(0xFF00F5FF);
+  static const Color neonPink = Color(0xFFFF3DF2);
+  static const Color neonYellow = Color(0xFFFFE600);
+  static const Color neonGreen = Color(0xFF29FF88);
+  
+  // Background Colors
+  static const Color backgroundDark = Color(0xFF050816);
+  static const Color backgroundDark2 = Color(0xFF0B1124);
+  static const Color surfaceDark = Color(0xFF111731);
+  static const Color surfaceDark2 = Color(0xFF182448);
+  
+  // Text Colors
+  static const Color textPrimary = Color(0xFFEAF2FF);
+  static const Color textMuted = Color(0xFF9FB2D0);
+  
+  // Border
+  static const Color borderLight = Color(0xFF1F1F1F).withOpacity(0.12);
+}
